7027151e89b743d7d50eadad613bc1976240b5d1
chivtn <chi.vtn.lly@gmail.com>
2025-05-11 23:15:57 +0700

    Chiiiii

--- QuanLyKyTucXa_main/FrmThongBaoSinhVien.Designer.cs
+++ QuanLyKyTucXa_main/FrmThongBaoSinhVien.Designer.cs
@@ -29,32 +29,53 @@
         private void InitializeComponent()
         {
             this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.txtNoidungThongbao = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtNoidungThongbao = new Guna.UI2.WinForms.Guna2TextBox();
             this.GBtnDong = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
             this.GBtnDongg = new Guna.UI2.WinForms.Guna2Button();
             this.GBtnGuiemail = new Guna.UI2.WinForms.Guna2Button();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.guna2CustomGradientPanel1.SuspendLayout();
-            this.GBtnDong.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.GBtnDong.SuspendLayout();
             this.SuspendLayout();
             // 
             // guna2CustomGradientPanel1
             // 
+            this.guna2CustomGradientPanel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.guna2CustomGradientPanel1.BorderRadius = 35;
+            this.guna2CustomGradientPanel1.Controls.Add(this.txtNoidungThongbao);
             this.guna2CustomGradientPanel1.Controls.Add(this.pictureBox2);
             this.guna2CustomGradientPanel1.Controls.Add(this.guna2HtmlLabel1);
             this.guna2CustomGradientPanel1.Controls.Add(this.pictureBox1);
             this.guna2CustomGradientPanel1.Controls.Add(this.label1);
-            this.guna2CustomGradientPanel1.Controls.Add(this.txtNoidungThongbao);
             this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(40, 48);
             this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
             this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(910, 512);
             this.guna2CustomGradientPanel1.TabIndex = 6;
+            // 
+            // txtNoidungThongbao
+            // 
+            this.txtNoidungThongbao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNoidungThongbao.Location = new System.Drawing.Point(110, 254);
+            this.txtNoidungThongbao.Multiline = true;
+            this.txtNoidungThongbao.Name = "txtNoidungThongbao";
+            this.txtNoidungThongbao.Size = new System.Drawing.Size(698, 214);
+            this.txtNoidungThongbao.TabIndex = 13;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Image = global::QuanLyKyTucXa_main.Properties.Resources.icons8_popup_100;
+            this.pictureBox2.Location = new System.Drawing.Point(110, 201);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 47);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
             // 
             // guna2HtmlLabel1
             // 
@@ -66,46 +87,36 @@
             this.guna2HtmlLabel1.TabIndex = 11;
             this.guna2HtmlLabel1.Text = "Nhập nội dung thông báo:";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Image = global::QuanLyKyTucXa_main.Properties.Resources.icons8_notifications_100;
+            this.pictureBox1.Location = new System.Drawing.Point(415, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 128);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(349, 134);
+            this.label1.Location = new System.Drawing.Point(349, 153);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(242, 25);
             this.label1.TabIndex = 8;
             this.label1.Text = "Thông Báo Cho SInh Viên";
             // 
-            // txtNoidungThongbao
-            // 
-            this.txtNoidungThongbao.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.txtNoidungThongbao.BorderRadius = 35;
-            this.txtNoidungThongbao.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtNoidungThongbao.DefaultText = "";
-            this.txtNoidungThongbao.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.txtNoidungThongbao.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.txtNoidungThongbao.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.txtNoidungThongbao.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.txtNoidungThongbao.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtNoidungThongbao.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNoidungThongbao.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtNoidungThongbao.Location = new System.Drawing.Point(83, 255);
-            this.txtNoidungThongbao.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.txtNoidungThongbao.Name = "txtNoidungThongbao";
-            this.txtNoidungThongbao.PlaceholderText = "";
-            this.txtNoidungThongbao.SelectedText = "";
-            this.txtNoidungThongbao.Size = new System.Drawing.Size(761, 228);
-            this.txtNoidungThongbao.TabIndex = 7;
-            // 
             // GBtnDong
             // 
+            this.GBtnDong.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.GBtnDong.BorderRadius = 25;
             this.GBtnDong.Controls.Add(this.GBtnDongg);
             this.GBtnDong.Controls.Add(this.GBtnGuiemail);
-            this.GBtnDong.Location = new System.Drawing.Point(494, 576);
+            this.GBtnDong.Location = new System.Drawing.Point(494, 595);
             this.GBtnDong.Name = "GBtnDong";
-            this.GBtnDong.Size = new System.Drawing.Size(456, 108);
+            this.GBtnDong.Size = new System.Drawing.Size(456, 130);
             this.GBtnDong.TabIndex = 70;
             // 
             // GBtnDongg
@@ -121,9 +132,9 @@
             this.GBtnDongg.Image = global::QuanLyKyTucXa_main.Properties.Resources.icons8_cancel_402;
             this.GBtnDongg.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.GBtnDongg.ImageSize = new System.Drawing.Size(40, 40);
-            this.GBtnDongg.Location = new System.Drawing.Point(248, 30);
+            this.GBtnDongg.Location = new System.Drawing.Point(246, 30);
             this.GBtnDongg.Name = "GBtnDongg";
-            this.GBtnDongg.Size = new System.Drawing.Size(157, 45);
+            this.GBtnDongg.Size = new System.Drawing.Size(180, 75);
             this.GBtnDongg.TabIndex = 70;
             this.GBtnDongg.Text = "   Đóng";
             this.GBtnDongg.Click += new System.EventHandler(this.GBtnDongg_Click);
@@ -141,49 +152,29 @@
             this.GBtnGuiemail.Image = global::QuanLyKyTucXa_main.Properties.Resources.icons8_gmail_logo_100;
             this.GBtnGuiemail.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.GBtnGuiemail.ImageSize = new System.Drawing.Size(40, 40);
-            this.GBtnGuiemail.Location = new System.Drawing.Point(62, 30);
+            this.GBtnGuiemail.Location = new System.Drawing.Point(30, 30);
             this.GBtnGuiemail.Name = "GBtnGuiemail";
-            this.GBtnGuiemail.Size = new System.Drawing.Size(157, 45);
+            this.GBtnGuiemail.Size = new System.Drawing.Size(188, 75);
             this.GBtnGuiemail.TabIndex = 69;
             this.GBtnGuiemail.Text = "Gửi email";
             this.GBtnGuiemail.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.GBtnGuiemail.Click += new System.EventHandler(this.GBtnGuiemail_Click);
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox2.Image = global::QuanLyKyTucXa_main.Properties.Resources.icons8_popup_100;
-            this.pictureBox2.Location = new System.Drawing.Point(110, 201);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(45, 47);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 12;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox1.Image = global::QuanLyKyTucXa_main.Properties.Resources.icons8_notifications_100;
-            this.pictureBox1.Location = new System.Drawing.Point(415, 22);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 93);
-            this.pictureBox1.TabIndex = 9;
-            this.pictureBox1.TabStop = false;
-            // 
             // FrmThongBaoSinhVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(990, 711);
+            this.BackColor = System.Drawing.Color.Plum;
+            this.ClientSize = new System.Drawing.Size(990, 753);
             this.Controls.Add(this.GBtnDong);
             this.Controls.Add(this.guna2CustomGradientPanel1);
             this.Name = "FrmThongBaoSinhVien";
             this.Text = "FrmThongBaoSinhVien";
             this.guna2CustomGradientPanel1.ResumeLayout(false);
             this.guna2CustomGradientPanel1.PerformLayout();
-            this.GBtnDong.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.GBtnDong.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -191,12 +182,12 @@
         #endregion
         private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
         private System.Windows.Forms.Label label1;
-        private Guna.UI2.WinForms.Guna2TextBox txtNoidungThongbao;
         private System.Windows.Forms.PictureBox pictureBox1;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
         private Guna.UI2.WinForms.Guna2Button GBtnGuiemail;
         private Guna.UI2.WinForms.Guna2CustomGradientPanel GBtnDong;
         private Guna.UI2.WinForms.Guna2Button GBtnDongg;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtNoidungThongbao;
     }
 }
